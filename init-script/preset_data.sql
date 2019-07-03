f21ed8e9bec1dbb2f7c56e7621f1df6ba5034035
yuetsin <akaza_akari@sjtu.edu.cn>
2019-07-03 17:12:56 +0800

    [add] `admins` sample data

---
+++ init-script/preset_data.sql
@@ -0,0 +1,5 @@
+USE eyulingo_db;
+
+INSERT INTO `eyulingo_db`.`Admins` (`admin_name`, `admin_password`) VALUES ('admin1', 'admin1password');
+INSERT INTO `eyulingo_db`.`Admins` (`admin_name`, `admin_password`) VALUES ('admin2', 'admin2password');
+INSERT INTO `eyulingo_db`.`Admins` (`admin_name`, `admin_password`) VALUES ('admin3', 'admin3password');
