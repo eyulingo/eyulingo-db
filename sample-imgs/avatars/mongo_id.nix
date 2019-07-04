63d16664108744a470322afbbd1b4c6599b2cd52
yuetsin <akaza_akari@sjtu.edu.cn>
2019-07-04 12:25:26 +0800

    [add] descriptions

--- sample-imgs/avatars/mongo_id.nix
+++ sample-imgs/avatars/mongo_id.nix
@@ -70,91 +70,180 @@ mongo_id.shop_cover rec {
 mongo_id.good_cover rec {
     shop_1 = {
         good_1 = "5d1d6ab66344590007151479";
+        # Name: iPhone 8 Plus
+
         good_2 = "5d1d6ac1634459000715147b";
+        # Name: iPad Pro (12.9-inch)
+
         good_3 = "5d1d6acc634459000715147d";
+        # Name: MacBook Pro (2019)
+
         good_4 = "5d1d6ad9634459000715147f";
+        # Name: Apple Watch (Series 4)
     };
 
     shop_2 = {
         good_1 = "5d1d6b3b6344590007151481";
+        # Name: Surface Pro 6
+
         good_2 = "5d1d6b476344590007151483";
+        # Name: Surface Book 2
+
         good_3 = "5d1d6b506344590007151485";
+        # Name: Surface Studio
+
         good_4 = "5d1d6b596344590007151487";
+        # Name: Nokia Lumia 1520
     };
 
     shop_3 = {
         good_1 = "5d1d6bba6344590007151489";
+        # Name: 芝士咖喱猪排饭
+
         good_2 = "5d1d6bd6634459000715148d";
+        # Name: 滑蛋厚切猪排饭
+
         good_3 = "5d1d6be0634459000715148f";
+        # Name: 爆浆猪排饭
+
         good_4 = "5d1d6bea6344590007151491";
+        # Name: 可口可乐（罐装）
     };
 
     shop_4 = {
         good_1 = "5d1d6c2b6344590007151493";
+        # Name: 奥尔良鸡排盖浇饭
+
         good_2 = "5d1d6c356344590007151496";
+        # Name: 大碗麻辣香锅饭
+
         good_3 = "5d1d6c416344590007151499";
+        # Name: 沙茶牛肉双拼饭
+
         good_4 = "5d1d6c51634459000715149b";
+        # Name: 百事可乐（罐装）
     };
 
     shop_5 = {
         good_1 = "5d1d6e80634459000715149d";
+        # Name: 奶油虾仁意大利面
+
         good_2 = "5d1d6e8c634459000715149f";
+        # Name: 咖喱鸡排饭团
+
         good_3 = "5d1d6ea363445900071514a1";
+        # Name: 超级海鲜披萨
+
         good_4 = "5d1d6eb363445900071514a3";
+        # Name: 海鲜烩炒饭
+
         good_5 = "5d1d6ebe63445900071514a5";
+        # Name: 麻婆豆腐烩饭
+
         good_6 = "5d1d6ecb63445900071514a7";
+        # Name: 炭烤风味鸡排便当
     };
 
     shop_6 = {
         good_1 = "5d1d6f1b63445900071514a9";
+        # Name: 钢笔
+
         good_2 = "5d1d6f2863445900071514ab";
+        # Name: 手表
+
         good_3 = "5d1d6f3463445900071514ae";
+        # Name: 墨水
     };
 
     shop_7 = {
         good_1 = "5d1d6fb263445900071514b0";
+        # Name: 华为 P30 Pro
+
         good_2 = "5d1d6fbf63445900071514b4";
+        # Name: 华为 MagicBook 2
+
         good_3 = "5d1d6fc963445900071514b6";
+        # Name: 华为手环 3
     };
 
     shop_8 = {
         good_1 = "5d1d703363445900071514b9";
+        # Name: 超能天然皂粉
+
         good_2 = "5d1d703e63445900071514bb";
+        # Name: 立白洗衣粉
+
         good_3 = "5d1d704663445900071514bd";
+        # Name: 去渍霸超能去污洗衣粉
+
         good_4 = "5d1d705263445900071514bf";
+        # Name: 汰渍洗衣液
+
         good_5 = "5d1d705e63445900071514c1";
+        # Name: 蓝月亮护理洗衣液
     };
 
     shop_9 = {
         good_1 = "5d1d710363445900071514c3";
+        # Name: 魅族 16
+
         good_2 = "5d1d711063445900071514c5";
+        # Name: 魅族 Note 6
+
         good_3 = "5d1d711a63445900071514c7";
+        # Name: 魅族 16X
     };
 
     shop_10 = {
         good_1 = "5d1d715d63445900071514c9";
+        # Name: 茄子绒布玩具
+
         good_2 = "5d1d716863445900071514cb";
+        # Name: 冰激凌球绒布玩具
+
         good_3 = "5d1d717263445900071514cd";
+        # Name: 海盐洁面乳
     };
 
     shop_11 = {
         good_1 = "5d1d71f363445900071514cf";
+        # Name: SNICKERS 士力架
+
         good_2 = "5d1d71fe63445900071514d1";
+        # Name: 苹果
+
         good_3 = "5d1d720b63445900071514d3";
+        # Name: 橙子
     };
 
     shop_12 = {
         good_1 = "5d1d729c63445900071514d5";
+        # Name: 金扶宁（外用重组人粒细胞巨噬细胞刺激因子凝胶剂）
+
         good_2 = "5d1d72a763445900071514d7";
+        # Name: 美罗华（利妥昔单抗注射液）
+
         good_3 = "5d1d72b263445900071514da";
+        # Name: 舒莱（巴利息单抗注射液）
+
         good_4 = "5d1d72bf63445900071514dc";
+        # Name: 拓益（特瑞普单抗注射液）
     };
 
     shop_13 = {
         good_1 = "5d1d732b63445900071514de";
+        # Name: Big Mac 巨无霸汉堡
+
         good_2 = "5d1d733a63445900071514e0";
+        # Name: 炸薯条
+
         good_3 = "5d1d734463445900071514e2";
+        # Name: McFlurry 麦旋风冰激凌
+
         good_4 = "5d1d734f63445900071514e4";
+        # Name: 双拼汉堡
+
         good_5 = "5d1d735963445900071514e6";
+        # Name: 炸面包条
     };
 }
